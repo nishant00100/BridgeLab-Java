13c05392294ab1b4a1e5500bc2c6bf886ed09132
Nishant Chauhan <chauhanjaat942@gmail.com>
2025-09-03 11:24:01 +0530

    ALL Problems Added

---
+++ Java Class and Object/level2/BankAccount.java
@@ -0,0 +1,40 @@
+class BankAccount {
+    String accountHolder;
+    int accountNumber;
+    double balance;
+
+    BankAccount(String accountHolder, int accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    void displayBalance() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount("Nishant", 12345, 5000);
+        acc.displayBalance();
+        acc.deposit(2000);
+        acc.withdraw(3000);
+        acc.displayBalance();
+    }
+}
+
